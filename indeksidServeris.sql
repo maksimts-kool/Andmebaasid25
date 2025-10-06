bf5b31f96d2771b2cf297061a683665203ad1fc0
maksimts-kool <maksimtsitkool@gmail.com>
2025-10-06 09:42:11 +0300

    Uuendatud indeksidServeris

--- indeksidServeris.sql
+++ indeksidServeris.sql
@@ -4,3 +4,7 @@ SELECT * FROM DimEmployee
 -- Loome indeksi, mis aitab päringut: Loome indeksi Salary veerule.
 CREATE INDEX IX_DimEmployee_BaseRate
 ON DimEmployee(BaseRate ASC)
+-- Kui soovid vaadata Indeksit
+EXEC sp_help DimEmployee;
+-- Kui soovid kustutada indeksit
+DROP INDEX DimEmployee.IX_DimEmployee_BaseRate
