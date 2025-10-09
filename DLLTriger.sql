9ea9ee21147fc119833fe817bd1b5c793eacb197
maksimts-kool <maksimtsitkool@gmail.com>
2025-10-09 14:56:49 +0300

    Lisatud DLLTriger - Süntaks

---
+++ DLLTriger.sql
@@ -0,0 +1,11 @@
+USE AdventureWorksDW2019
+
+-- DLL Trigerid serveris
+-- Süntaks loomaks DDL trigereid
+CREATE TRIGGER [Trigger_Name]
+ON [Scope(Server/Database)]
+FOR [EventType1, EventType2, EventType3]
+AS
+BEGIN
+-- Trigger Body
+END
