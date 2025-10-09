089cb812591849c0213ac0d45b63426d9b3c1eb2
maksimts-kool <maksimtsitkool@gmail.com>
2025-10-09 14:58:24 +0300

    Uuendatud DLLTriger - FirstTrigger

--- DLLTriger.sql
+++ DLLTriger.sql
@@ -9,3 +9,14 @@ AS
 BEGIN
 -- Trigger Body
 END
+
+-- Järgnev trigger käivitab vastuseks CREATE_TABLE DDL sündmuse: sp_rename
+CREATE TRIGGER FirstTrigger
+ON Database
+FOR CREATE_TABLE
+AS
+BEGIN
+PRINT 'New table created'
+END
+-- Kui sa järgnevat koodi käivitad, siis trigger läheb automaatselt käima ja prindib välja sõnumi: uus tabel on loodud.
+CREATE TABLE test (id INT)
